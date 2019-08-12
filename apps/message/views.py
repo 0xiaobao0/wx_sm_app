0fd7a0ec9aba2b3b052e6c09dda1286d93323123
0xiaobao0 <31815318+0xiaobao0@users.noreply.github.com>
2019-08-12 18:31:25 +0800

    Update views.py

--- apps/message/views.py
+++ apps/message/views.py
@@ -20,13 +20,13 @@ class GetMyMessages(APIView):
             page_message_list = message_Page.paginate_queryset(queryset=myMessages, request=request,
                                                                view=self)  # 把数据放在分页器上面
             serializer = UserMessageSerrializer(instance=page_message_list, many=True)  # 序列化数据
-            res = BaseResponse(code='1043', msg='获取我的消息列表成功',
+            res = BaseResponse(code='200', msg='获取我的消息列表成功',
                                data=serializer.data)
             res.next = message_Page.get_next_link()
             return Response(res.result)
         except Exception as e:
             print(e)
-            return Response(BaseResponse(code='1413', msg='获取我的消息列表失败，请稍后再试').result)
+            return Response(BaseResponse(code='500', msg='获取我的消息列表失败，请稍后再试').result)
 
 
 
